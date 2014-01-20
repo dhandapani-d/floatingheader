4fb43434739343695e93d9a1cad3a21c7dbdd013
Dhandapani Devarajan <dhandapani.kgp@gmail.com>
2014-01-20 15:02:37 +0530

    Initial Commit - Empty Project

---
+++ FloatingHeader1/DDFloatingHeader/DDFloatingHeader/DDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDAppDelegate.h
+//  DDFloatingHeader
+//
+//  Created by Dhandapani on 20/01/14.
+//  Copyright (c) 2014 Dhandapani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FloatingHeader1/DDFloatingHeader/DDFloatingHeader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DDFloatingHeader
+//
+//  Created by Dhandapani on 20/01/14.
+//  Copyright (c) 2014 Dhandapani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DDAppDelegate class]));
+    }
+}
